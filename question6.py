7efcf227354af2adb4aae4ace55b58140bf02afb
Christian Cardozo <chriiscardozo@gmail.com>
2017-11-16 01:54:47 -0200

    choosing unique model that generate pdf

--- question6.py
+++ question6.py
@@ -1,6 +1,5 @@
 from read_data import train, test, mask
 import numpy as np
-from Regression import Regression
 from ACSM import ACSM
 from sklearn.mixture import GaussianMixture
 from scipy.stats import multivariate_normal
@@ -11,7 +10,7 @@ def predict_mvn(X, rv, min, max, N):
 		sum_prob = 0
 		soma = 0
 		for v in np.linspace(min, max, N):
-			proba = rv.pdf([x[0],x[1], x[2], v])
+			proba = rv.pdf(x[0:-1].tolist() + [v])
 			soma += (v * proba)
 			sum_prob += proba
 		y_pred.append(soma/sum_prob)
@@ -36,69 +35,31 @@ def carregar_dataset(X_fields, y_field):
 
 	return [X, y, X_test, y_test]
 
-X, y, X_test, y_test = carregar_dataset(['peso','carga'], 'vo2max')
 
 # *** MODELO ACSM ***
+X, y, X_test, y_test = carregar_dataset(['peso','carga'], 'vo2max')
 print("ACSM")
 clf_acsm = ACSM()
 y_pred = clf_acsm.predict(X_test)
 print("\tmse:", clf_acsm.mse_error(y_test,y_pred))
 print("\tscore:", clf_acsm.score(y_test,y_pred))
 
-# *** MODELO REGRESSÃO D=2 VARS INDEPENDENTES ***
-print("Regressão 1 (sem dep)")
-modelo = Regression()
-modelo.phi_inter = lambda X, d: np.array([[1, x[0], x[1], x[0]**2, x[1]**2] for x in X])
-modelo.fit(X, y)
-y_pred = modelo.predict(X_test)
 
-print("\tmse:", modelo.mse_error(y_test, y_pred))
-print("\tscore:", modelo.score(y_test, y_pred))
-print("\tw:", [str(round(x, 3)) for x in modelo.W])
-
-# *** MODELO REGRESSÃO D=2 DEPENDENCIA X*Y ***
-print("Regressão 2 (com dep)")
-modelo = Regression()
-modelo.phi_inter = lambda X, d: np.array([[1, x[0], x[1], x[0]*x[1], x[0]**2, x[1]**2, (x[0]**2)*x[1], (x[1]**2)*x[0], (x[0]**2)*(x[1]**2), x[0]/x[1]] for x in X])
-modelo.fit(X, y)
-y_pred = modelo.predict(X_test)
-
-print("\tmse:", modelo.mse_error(y_test, y_pred))
-print("\tscore:", modelo.score(y_test, y_pred))
-print("\tw:", [str(round(x, 3)) for x in modelo.W])
-
-X, y, X_test, y_test = carregar_dataset(['peso','carga','idade'], 'vo2max')
-
-# *** Regressão 3 vars D=3 com dep ***
-print("Regressão 3 (3 vars c/ dep)")
-modelo = Regression()
-modelo.phi_inter = lambda X, d: np.array([[1, x[0], x[1], 1/x[2], x[0]*x[1], x[0]**2, x[1]**2, (x[0]**2)*x[1], (x[1]**2)*x[0], (x[0]**2)*(x[1]**2), x[0]/x[1]] for x in X])
-modelo.fit(X, y)
-y_pred = modelo.predict(X_test)
-
-print("\tmse:", modelo.mse_error(y_test, y_pred))
-print("\tscore:", modelo.score(y_test, y_pred))
-print("\tw:", [str(round(x, 3)) for x in modelo.W])
-
+# *** GAUSSIANA MULTIVARIADA (peso,carga,idade,vo2max) ***
 X, y, X_test, y_test = carregar_dataset(['peso','carga','idade', 'vo2max'], 'vo2max')
-
-# *** GAUSSIANA MULTIVARIADA ***
-print("Gaussiana Multivariada")
-#Parameters to set
+print("MVN[peso,carga,idade,vo2max]")
 mu_x = X[:,0].mean()
 mu_y = X[:,1].mean()
 mu_z = X[:,2].mean()
 mu_k = X[:,3].mean()
 cov_matrix = np.cov(X.transpose())
-
 rv = multivariate_normal([mu_x, mu_y, mu_z, mu_k], cov_matrix)
+y_pred = predict_mvn(X_test, rv, 0, 100, 100)
 
 print("\tPeso - Media:", mu_x)
 print("\tCarga - Media:", mu_y)
 print("\tIdade - Media:", mu_z)
 print("\tVO2MAX - Media:", mu_k)
 print("\tCov Mat:\n", cov_matrix)
-
-y_pred = predict_mvn(X_test, rv, 0, 100, 100)
 print("\tmse:", mse_error(y_test, y_pred))
 print("\tscore:", score(y_test, y_pred))
